786189a85e32932b9fb8e7d455b845c179cb51dc
Diogo dos Santos <90933647+diog0bjj@users.noreply.github.com>
2022-01-26 16:28:19 -0300

    Estudando loopings

---
+++ script.js
@@ -0,0 +1,17 @@
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
+
+let i=11;
+while(i>=2){
+    i--
+    console.log(i)
+}
+
+for(let i=1; i<=100; i=i+2){
+    console.log(i)
+}
+
+for(let i=0; i<=100; i=i+2){
+    console.log(i)
+}
